8bb5add42e431ab4d226673094743e516b513687
Cian747 <studiokaiser1@gmail.com>
2025-06-29 12:14:54 +0300

    testing the api endpoint

--- short/models.py
+++ short/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ShortURL(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=6, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    clicks = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.short_code} → {self.original_url}"
+
--- short/views.py
+++ short/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -11,10 +9,13 @@ from .models import ShortURL
 
 class ShortenURL(APIView):
     def post(self, request):
-        original_url = request.data.get('url')
-        short_code = generate_short_code()
-        short_url = ShortURL.objects.create(original_url=original_url, short_code=short_code)
-        return Response({"short_url": f"http://localhost:8000/{short_code}"}, status=status.HTTP_201_CREATED)
+        if request.method == "POST":
+            original_url = request.POST.get("url")
+            print(original_url)
+            short_code = generate_short_code()
+            print(short_code)
+            short_url = ShortURL.objects.create(original_url=original_url, short_code=short_code)
+            return Response({"short_url": f"http://localhost:8000/{short_code}"}, status=status.HTTP_201_CREATED)
 
 def redirect_view(request, short_code):
     try:
