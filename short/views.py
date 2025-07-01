556250c66b9bf58baebb290f7cbb0d2507fd3fd1
Cian747 <studiokaiser1@gmail.com>
2025-07-01 10:23:05 +0300

    testing different url shortener function

--- short/views.py
+++ short/views.py
@@ -6,16 +6,59 @@ from .models import ShortURL
 from .utils import generate_short_code
 from django.shortcuts import redirect
 from .models import ShortURL
+import requests
+from django.http import JsonResponse
+from .serializers import URLSerializer
 
-class ShortenURL(APIView):
-    def post(self, request):
-        if request.method == "POST":
-            original_url = request.POST.get("url")
-            print(original_url)
-            short_code = generate_short_code()
-            print(short_code)
-            short_url = ShortURL.objects.create(original_url=original_url, short_code=short_code)
-            return Response({"short_url": f"http://localhost:8000/{short_code}"}, status=status.HTTP_201_CREATED)
+# class ShortenURL(APIView):
+#     def post(self, request):
+#         serializer = URLSerializer(data = request.data)
+#         if serializer.is_valid():
+#             original_url = serializer.validated_data['url']
+#             print(original_url)
+#             short_code = generate_short_code()
+#             print(short_code)
+#             short_url = ShortURL.objects.create(original_url=original_url, short_code=short_code)
+#             return Response({"short_url": f"http://localhost:8000/{short_code}"}, status=status.HTTP_201_CREATED)
+        
+#         if not serializer.is_valid():
+#             print("Validation errors:", serializer.errors)
+#             return Response(serializer.errors, status=400)
+
+
+        # # Ensure we are sending JSON with correct headers
+        # api_response = requests.post(
+        #     "http://localhost:8000/api/shorten/",  # or use your domain
+        #     json={"url": original_url},
+        #     headers={"Content-Type": "application/json"}
+        # )
+
+        # try:
+        #     data = api_response.json()
+        # except ValueError:
+        #     return JsonResponse({"error": "Invalid response from API"}, status=500)
+
+        # return JsonResponse(data)
+
+def shorten_via_api(request):
+    if request.method == "POST":
+        original_url = request.POST.get("url")
+
+        # Ensure we are sending JSON with correct headers
+        api_response = requests.post(
+            "http://localhost:8000/api/shorten/",  # or use your domain
+            json={"url": original_url},
+            headers={"Content-Type": "application/json"}
+        )
+
+        try:
+            data = api_response.json()
+        except ValueError:
+            return JsonResponse({"error": "Invalid response from API"}, status=500)
+
+        return JsonResponse(data)
+
+    return render(request, "shorten.html")
 
 def redirect_view(request, short_code):
     try:
